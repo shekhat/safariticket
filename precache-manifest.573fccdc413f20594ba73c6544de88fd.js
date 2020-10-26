e0865ad16d7a6184db748d54f3d63bee9b49b3c8
Shekhat <shekhatazizi@gmail.com>
2020-10-26 13:19:15 +0300

    Updates

---
+++ precache-manifest.573fccdc413f20594ba73c6544de88fd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16453ae143741fc772a94600233dbb3b",
+    "url": "/safariticket/index.html"
+  },
+  {
+    "revision": "91b16451641cbdd96c68",
+    "url": "/safariticket/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "3efdf69bbcd7f41b8932",
+    "url": "/safariticket/static/css/main.528236cc.chunk.css"
+  },
+  {
+    "revision": "91b16451641cbdd96c68",
+    "url": "/safariticket/static/js/2.152b51a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/safariticket/static/js/2.152b51a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3efdf69bbcd7f41b8932",
+    "url": "/safariticket/static/js/main.2cc59543.chunk.js"
+  },
+  {
+    "revision": "aa390485854a3ff4f0d4",
+    "url": "/safariticket/static/js/runtime-main.27af5f6f.js"
+  },
+  {
+    "revision": "44aebabfb41b700f96c2b09f03e75e19",
+    "url": "/safariticket/static/media/Accommodation.44aebabf.jpg"
+  },
+  {
+    "revision": "f8631abd632edcf7d52087488b8ae5ce",
+    "url": "/safariticket/static/media/Airplane2.f8631abd.jpg"
+  },
+  {
+    "revision": "59fdb0cb723141c5cbb1de3ea076ecd7",
+    "url": "/safariticket/static/media/Boat2.59fdb0cb.jpg"
+  },
+  {
+    "revision": "1325f73421425cef86b3e164efd2c999",
+    "url": "/safariticket/static/media/Bus2.1325f734.png"
+  },
+  {
+    "revision": "a9437ce628ae8fe64a88b5ff5942cf7e",
+    "url": "/safariticket/static/media/Food.a9437ce6.jpg"
+  },
+  {
+    "revision": "3fa4a3d98da27154da26f2fc0d3bc5ec",
+    "url": "/safariticket/static/media/Taxi.3fa4a3d9.jpg"
+  },
+  {
+    "revision": "24167045c1643ea2472a4b14c1385d35",
+    "url": "/safariticket/static/media/Train2.24167045.jpg"
+  },
+  {
+    "revision": "7de2b364722c72625f8119c1aa51fd5e",
+    "url": "/safariticket/static/media/logo.7de2b364.png"
+  }
+]);
